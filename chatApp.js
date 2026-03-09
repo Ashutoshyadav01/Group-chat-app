93135cabcdf1ae32da081c3a557db811bed6c160
Ashutoshyadav01 <ashuyadav5028246@gmail.com>
2026-03-10 01:58:31 +0530

    adding port number for render hosting

--- chatApp.js
+++ chatApp.js
@@ -27,9 +27,11 @@ io.on("connection", (socket) => {
 
 })
 
-server.listen(3000, () => {
-  console.log("server is live on 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("server is live on", PORT);
+});
 
 
 
